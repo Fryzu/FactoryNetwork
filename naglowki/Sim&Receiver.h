22fb2a523e59d523df69dac1cb64493f7cfd0817
MrauMrauChan <kwajda2005@gmail.com>
2018-01-12 18:45:54 +0100

    git push

---
+++ naglowki/Sim&Receiver.h
@@ -0,0 +1,22 @@
+class Simulation
+{
+private:
+    std::vector<Storehouse> storehouses;
+    std::vector<Worker>workers;
+    std::vector<LoadingRamp>loadingRamps;
+
+public:
+    void simulate(std::string);
+    void interpretateFile(std::string);
+    string generateRaport(std::string);
+    Simulation()    //Konstruktor!!!!//
+
+};
+
+class ProductReceiver
+{
+public:
+   virtual receiveProduct(Product)=0;
+   virtual ~ProductReceiver()=default;
+
+};
